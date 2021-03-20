0e1bedb053480ddc6a5b49733f2b5d6d1bce3fbd
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2021-03-20 23:35:57 +0530

    Update Interface_concept.java

--- Interface_concept.java
+++ Interface_concept.java
@@ -1,11 +1,7 @@
 interface Myinterface{
-    int i = 5;                                    // By default i is final, it can't be modified.
+    int i = 5;                                   
     void meth1();
-    void meth2();
-
-//    void meth3(){
-//        System.out.println("implemented");      // Interface abstract methods cannot have body(Implementation).
-//    }                                           // Interface cannot be instantiated.
+    void meth2();     
 }
 
 class NewClass implements Myinterface{
@@ -23,9 +19,8 @@ class NewClass implements Myinterface{
 public class Interface_concept {
     public static void main(String[] args) {
 //   Myinterface a = new Myinterface();     // It will give me an error (Myinterface is abstract and cannot be instantiated)
-      Myinterface n = new NewClass();        // we can create reference of Myinterface .
-       // n.i = 10;                          // It will give you an error.
-         //  n.meth3();              // It will give me an error - Cannot resolve meth3() in Myinterface.
+      Myinterface n = new NewClass();        
+                   
          }
     }
 
