4a97edd7ac4098705178ad19f19ef36b309d8dd8
rishabhsharma015 <60418660+rishabhsharma015@users.noreply.github.com>
2021-03-26 23:52:53 +0530

    Update SinglyLinkedList.java
    Done

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -10,7 +10,7 @@ public class SinglyLinkedList {
             this.next = null;
         }
     }
-    public void printLinkedList(){
+    public void printLinkedList(){                                            // Printing Linked List
         Node p = head;
         while(p.next!=null){
             System.out.print(p.data+" ");
@@ -19,12 +19,12 @@ public class SinglyLinkedList {
         System.out.print(p.data);
     }
 
-    public void insertionAtFront(Node newnode){
+    public void insertionAtFront(Node newnode){                               // Inserting a new Node at beginning
         newnode.next = head;
         head = newnode;
     }
 
-    public void insertionAtEnd(Node newnode){
+    public void insertionAtEnd(Node newnode){                                 // Inserting a new Node at End
         Node p = head;
         while(p.next != null){
             p = p.next;
@@ -32,7 +32,7 @@ public class SinglyLinkedList {
         p.next = newnode;
     }
 
-    public void insertionAtSpecificPostion(Node newnode, int pos){
+    public void insertionAtSpecificPostion(Node newnode, int pos){            // Inserting a new Node at Specific Postion
         Node p = head;
         int count=1;
         while(count < pos-1){
@@ -43,18 +43,52 @@ public class SinglyLinkedList {
         p.next = newnode;
         newnode.next = d;
     }
+    
+    public void deleteFromFront(){                                           // Deleting a Node from Front
+        if(head == null){
+            System.out.println("List is Empty");
+        }
+        else {
+            head = head.next;
+        }
+    }
+
+    public void deleteFromEnd(){                                             // Deleting a Node from End
+        Node p = head;
+        if(head == null){
+            System.out.println("Can't delete, list is Empty");
+        }
+        else {
+            while (p.next.next != null) {
+                p = p.next;
+            }
+            p.next = null;
+        }
+    }
+
+    public void deleteFromSpecificPostion(int pos){                        // Deleting a Node from specific Postion.
+        Node p = head;
+        Node d;
+        int count = 1;
+        while(count < pos-1){
+            p = p.next;
+            count++;
+        }
+        d = p.next;
+        p.next = d.next;
+    }
 
     public static void main(String[] args) {
         SinglyLinkedList s = new SinglyLinkedList();
 
-        // Creating Nodes
+                                                                           // Creating Nodes
         s.head = new Node(10);
         Node n2 = new Node(20);
         Node n3 = new Node(30);
         Node n4 = new Node(40);
         Node n5 = new Node(50);
 
-        // Linking Nodes
+                                                                           // Linking Nodes
         s.head.next = n2;
         n2.next = n3;
         n3.next = n4;
